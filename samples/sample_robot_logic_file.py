c1415f1893bdc3e07da41395016f8bc741fd1154
shimamura <kaito.shimamura.notat@gmail.com>
2024-12-05 21:33:06 +0900

    サンプルのロジックPythonファイルと結果JSONファイルを追加

---
+++ samples/sample_robot_logic_file.py
@@ -0,0 +1,17 @@
+# 必須のrobot_logic関数を定義
+def robot_logic(robot, game_ifo):
+    # スタミナが少ない場合は回避、それ以外は敵に近づいて攻撃
+    enemy_position = game_ifo['enemy_position']
+    if robot.sp < 20:
+        return "evade"
+    elif abs(robot.position[0] - enemy_position[0]) + abs(robot.position[1] - enemy_position[1]) == 1:
+        return "attack"
+    else:
+        if robot.position[0] < enemy_position[0]:
+            return "right"
+        elif robot.position[0] > enemy_position[0]:
+            return "left"
+        elif robot.position[1] < enemy_position[1]:
+            return "down"
+        else:
+            return "up"
